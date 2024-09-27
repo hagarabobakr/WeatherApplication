fb58131c46461e47cbad92d7228fa3d8221fb722
Hager Abobakr <hagarabobakr1@gmail.com>
2024-09-27 12:51:17 +0300

    Complete class WeatherLocalDataSource

--- app/src/main/java/com/example/weatherapp/data/local/WeatherLocalDataSource.kt
+++ app/src/main/java/com/example/weatherapp/data/local/WeatherLocalDataSource.kt
@@ -1,4 +1,39 @@
 package com.example.weatherapp.data.local
 
-class WeatherLocalDataSource {
+import com.example.weatherapp.data.model.Weather
+import com.example.weatherapp.data.model.WeatherForecast
+import kotlinx.coroutines.flow.Flow
+
+class WeatherLocalDataSource(private val weatherDao: WeatherDao) {
+    suspend fun insertWeather(weather: Weather) {
+        weatherDao.insertWeather(weather)
+    }
+
+    suspend fun getWeatherById(weatherId: Long): Weather? {
+        return weatherDao.getWeatherById(weatherId)
+    }
+
+    fun getAllWeather(): Flow<List<Weather>> {
+        return weatherDao.getAllWeather()
+    }
+
+    suspend fun insertWeatherForecast(forecast: WeatherForecast) {
+        weatherDao.insertWeatherForecast(forecast)
+    }
+
+    suspend fun getForecastByDate(forecastDate: Long): WeatherForecast? {
+        return weatherDao.getForecastByDate(forecastDate)
+    }
+
+    fun getAllWeatherForecast(): Flow<List<WeatherForecast>> {
+        return weatherDao.getAllWeatherForecast()
+    }
+
+    suspend fun deleteAllWeather() {
+        weatherDao.deleteAllWeather()
+    }
+
+    suspend fun deleteAllWeatherForecast() {
+        weatherDao.deleteAllWeatherForecast()
+    }
 }
