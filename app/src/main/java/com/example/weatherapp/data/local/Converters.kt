488b46299b81b4795d4f039663357b5f0a7cb790
Hager Abobakr <hagarabobakr1@gmail.com>
2024-09-27 12:30:45 +0300

    class Converters

--- app/src/main/java/com/example/weatherapp/data/local/Converters.kt
+++ app/src/main/java/com/example/weatherapp/data/local/Converters.kt
@@ -1,4 +1,21 @@
 package com.example.weatherapp.data.local
 
+import androidx.room.TypeConverter
+import com.example.weatherapp.data.model.WeatherElement
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 class Converters {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromWeatherElementList(value: List<WeatherElement>?): String? {
+        return gson.toJson(value)
+    }
+
+    @TypeConverter
+    fun toWeatherElementList(value: String?): List<WeatherElement>? {
+        val listType = object : TypeToken<List<WeatherElement>>() {}.type
+        return gson.fromJson(value, listType)
+    }
 }
